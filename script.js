cbbdb8eaec25764c990b7bb57f91da27351e8db6
noelig1 <166657339+noelig1@users.noreply.github.com>
2024-05-17 19:47:33 -0300

    tarea 7 se corrigio

---
+++ script.js
@@ -0,0 +1,15 @@
+let name = "Pedro" ;
+let years = 30 ;
+console.log (years) ;
+console.log ()
+
+let numberone=120;
+let numbertwo=20;
+let resultsuma = numberone + numbertwo;
+let resulresta = numberone - numbertwo;
+let resulmulti = numberone * numbertwo;
+let resuldivision = numberone / numbertwo;
+console.log("suma:",resultsuma);
+console.log("resta:",resulresta);
+console.log("multiplicacion:",resulmulti);
+console.log("division:",resuldivision);
