ccd2ee818ced0003ca559cea6e2d1af3825976be
noelig1 <166657339+noelig1@users.noreply.github.com>
2024-05-18 17:35:37 -0300

    tarea 10 completa

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let name = "Pedro" ;
-let years = 30 ;
-console.log (years) ;
+/*let name = "Pedro";
+let years = 30;
+console.log (years);
 console.log ()
 
 let numberone=120;
@@ -13,3 +13,24 @@ console.log("suma:",resultsuma);
 console.log("resta:",resulresta);
 console.log("multiplicacion:",resulmulti);
 console.log("division:",resuldivision);
+*/
+
+/*TAREA 10*/
+let nombre = "noeli";
+let apellido = "gaceo";
+console.log(nombre +" " + apellido)
+
+let numero1 = 50;
+let numero2 = 150;
+let suma = numero1 + numero2;
+
+console.log(
+    "El numero 1 es"  + numero1 + 
+    "y el numero 2 es" + numero2 +
+    "y el resultado es" + suma
+);
+
+let sumaVariables = suma + 40;
+
+console.log(sumaVariables);
+
