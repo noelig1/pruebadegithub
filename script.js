970e7f931a69067ed79ef46935621a8408341610
noelig1 <166657339+noelig1@users.noreply.github.com>
2024-05-26 16:36:41 -0300

    tarea 11 finalizada en js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ console.log("division:",resuldivision);
 */
 
 /*TAREA 10*/
-let nombre = "noeli";
+/*let nombre = "noeli";
 let apellido = "gaceo";
 console.log(nombre +" " + apellido)
 
@@ -33,4 +33,102 @@ console.log(
 let sumaVariables = suma + 40;
 
 console.log(sumaVariables);
+*/
+/*let nota = 10;
+if (nota < 4) {
+    console.log ("aplazado");
+} else if (nota < 7) {
+    console.log ("desaprobado");
+} else if (nota < 9) {
+    console.log ("aprobado");
+} else {
+    console.log ("sobresaliente");
+}
 
+let nombres = ["Diego", "Anabel", "Esteban"];
+console.log (nombres);
+console.log (nombres [0]);
+console.log (nombres [2]);
+console.log ("cantidad de nombre:", nombres.length);
+for (let i = 0; i< nombres.length; i++) {
+    console.log(nombres[i])
+}
+//
+*/
+//TAREA 11//
+let topping ="oreo"
+let precio = 0; 
+let precioFinal = 0;
+let helado = 1500;
+if (topping == "oreo") {
+    precio = 10;
+} else if(topping == "kitkat") {
+    precio = 15;
+} else if(topping == "kinder") {
+    precio = 25;
+} else {console.log("no tenemos este topping");
+}
+precioFinal = helado + precio;
+console.log("el helado cuesta" + precioFinal) 
+
+//SEGUNDA PARTE DE LA TAREA 11//
+let menu = "Pescado";
+switch(menu) {
+    case "Carne":
+        console.log("Se le ofrece como bebida vino.");
+        break;
+    case "Pescado":
+        console.log("Se le ofrece como bebida vino blanco.");
+        break;
+    case "Verdura":
+        console.log("Se le ofrece como bebida agua.");
+        break;
+    default:
+        console.log("Elija Carne, Pescado o Verdura.");
+        break;
+}
+
+
+
+
+
+/*
+// Declarar variables
+let num = 1;  // Número inicial
+let sum = 0;    // Variable para almacenar la suma
+
+// Bucle while
+while (num <= 10) {
+  sum += num;  // Sumar el valor actual de 'numero' a 'suma'
+  num++;        // Incrementar 'numero' en 1
+}
+/*
+// Mostrar el resultado
+console.log("La suma de los números del 1 al 10 es: " + sum);
+
+let result:
+let n1 = 3:
+let n2 = 5:
+result = n1 + n2
+function sumas_y_muestra() {
+    alert("el resultado es" + result)
+} */
+/*le falta terminarrrr*/
+/*
+// Solicita al usuario que introduzca un número entero
+let nu = prompt("Introduce un número entero");
+
+// Llama a la función parImpar y guarda el resultado
+let resulta = parImpar(nu);
+
+// Muestra el resultado en una alerta
+alert("El número " + nu + " es " + resulta);
+
+// Define la función parImpar
+function parImpar(nu) {
+  if (nu % 2 == 0) {
+    return "par";
+  } else {
+    return "impar";
+  }
+} */
